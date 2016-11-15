6afc445ee45617c32a83836ab288bb68b4e50b5b
Ajith <ajithv@axosoft.com>
2016-11-15 14:53:22 -0700

    webapp added

---
+++ dv-web-app/WebApp/UtilityLocal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebApp
+{
+    public static class UtilityLocal
+    {
+        public static void TransformFile(string filePath, string newFilePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            using (var writeFile = new StreamWriter(newFilePath, true))
+            {
+                writeFile.WriteLine(lines[0]);
+                for (var i = 1; i < lines.Count() ; i +=2)
+                {
+                    var temp = lines[i] + lines[i + 1];
+                    writeFile.WriteLine(temp);
+                }
+            }
+        }
+
+
+        public static void CleanTransformedFile(string filePath, string newFilePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            using (var writeFile = new StreamWriter(newFilePath, true))
+            {
+                writeFile.WriteLine(lines[0]);
+                for (var i = 1; i < lines.Count(); i ++)
+                {
+                    var temp = lines[i].Split('"');
+                    temp[1] = temp[1].Replace(',', ' ');
+                    writeFile.WriteLine(String.Join("\"", temp));
+                }
+            }
+        }
+    }
+}
---
+++ dv-web-app/WebApp/js/custom.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    Modernizr.load({
+        test: Modernizr.svg,
+        yep: 'js/network.js',
+        nope: ['js/jquery-1.9.1.min.js', 'js/fallback.js']
+    });
+    loadNetwork(2008);
+    $('#yearInTitle').html(2008);
+    $('#yearSelector button').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        
+        $('#yearInTitle').html($(this).html());
+        $('#networkChart').html('');
+        loadNetwork($(this).html());
+    });
+});
